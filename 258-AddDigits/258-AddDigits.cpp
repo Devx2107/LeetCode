9e3d8b4a77caa451c658342e2a60014dcdce3756
Devx2107 <96907979+Devx2107@users.noreply.github.com>
2026-05-21 18:11:43 +0530

    Create 258-AddDigits

---
+++ 258-AddDigits/258-AddDigits.cpp
@@ -0,0 +1,18 @@
+// Last updated: 21/05/2026, 18:11:48
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        if(num < 10)
+            return num;
+
+        int sum = 0;
+
+        while(num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return addDigits(sum);
+    }
+};
