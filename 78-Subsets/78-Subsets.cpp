2cd299c64f3ecd2d2953d4359d9cfbf851bc1d58
Devx2107 <96907979+Devx2107@users.noreply.github.com>
2026-05-21 18:13:19 +0530

    Create 78-Subsets

---
+++ 78-Subsets/78-Subsets.cpp
@@ -0,0 +1,21 @@
+// Last updated: 21/05/2026, 18:13:24
+class Solution {
+public:
+    void backtrack(vector<int>& nums, int start, vector<int>& curr, vector<vector<int>>& res) {
+        res.push_back(curr); 
+
+        for(int i = start; i < nums.size(); i++) {
+            
+            curr.push_back(nums[i]);
+            backtrack(nums, i + 1, curr, res);
+            curr.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> curr;
+        backtrack(nums, 0, curr, res);
+        return res;
+    }
+};
