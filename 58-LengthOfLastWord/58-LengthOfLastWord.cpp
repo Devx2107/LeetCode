d914303c40cf28f9483dbdd3ec598c4f1c27b401
Devx2107 <96907979+Devx2107@users.noreply.github.com>
2026-05-21 18:13:32 +0530

    Create 58-LengthOfLastWord

---
+++ 58-LengthOfLastWord/58-LengthOfLastWord.cpp
@@ -0,0 +1,20 @@
+// Last updated: 21/05/2026, 18:13:37
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+        int i = s.size() - 1;
+        int len = 0;
+
+        
+        while(i >= 0 && s[i] == ' ')
+            i--;
+        
+        while(i >= 0 && s[i] != ' ') {
+            len++;
+            i--;
+        }
+
+        return len;
+    }
+};
