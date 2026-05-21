6db90c5d6a5c3e818a5a32403850ea73f2666538
Devx2107 <96907979+Devx2107@users.noreply.github.com>
2026-05-21 18:11:36 +0530

    Create 268-MissingNumber

---
+++ 268-MissingNumber/268-MissingNumber.cpp
@@ -0,0 +1,14 @@
+// Last updated: 21/05/2026, 18:11:41
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int res = nums.size();
+
+        for(int i = 0; i < nums.size(); i++) {
+            res ^= i ^ nums[i];
+        }
+
+        return res;
+    }
+};
