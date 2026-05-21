0b6a1d96b4b441d896e6a68a772606251a3d8dd4
Devx2107 <96907979+Devx2107@users.noreply.github.com>
2026-05-21 18:09:59 +0530

    Create 4247-MinimumCapacityBox

---
+++ 4247-MinimumCapacityBox/4247-MinimumCapacityBox.cpp
@@ -0,0 +1,17 @@
+// Last updated: 21/05/2026, 18:10:04
+class Solution {
+public:
+    int minimumIndex(vector<int>& capacity, int itemSize) {
+        int minCap = INT_MAX;
+        int idx = -1;
+
+        for (int i = 0; i < capacity.size(); i++) {
+            if (capacity[i] >= itemSize && capacity[i] < minCap) {
+                minCap = capacity[i];
+                idx = i;
+            }
+        }
+
+        return idx;
+    }
+};
