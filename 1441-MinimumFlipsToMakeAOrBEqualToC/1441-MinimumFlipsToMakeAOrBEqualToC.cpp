fd522097aeef9ab011f91c8d156225b1f88ff085
Devx2107 <96907979+Devx2107@users.noreply.github.com>
2026-05-21 18:10:26 +0530

    Create 1441-MinimumFlipsToMakeAOrBEqualToC

---
+++ 1441-MinimumFlipsToMakeAOrBEqualToC/1441-MinimumFlipsToMakeAOrBEqualToC.cpp
@@ -0,0 +1,28 @@
+// Last updated: 21/05/2026, 18:10:31
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        
+        int flips = 0;
+
+        while(a > 0 || b > 0 || c > 0) {
+            
+            int abit = a & 1;
+            int bbit = b & 1;
+            int cbit = c & 1;
+
+            if(cbit == 1) {
+                if(abit == 0 && bbit == 0)
+                    flips += 1;
+            } else {
+                flips += abit + bbit;
+            }
+
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+        }
+
+        return flips;
+    }
+};
