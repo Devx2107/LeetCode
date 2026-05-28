ccca4bfb45fcdc8b19b33ec493f79a2bb9d24e3a
Devx2107 <96907979+Devx2107@users.noreply.github.com>
2026-05-28 14:00:51 +0530

    Create 907-KokoEatingBananas

---
+++ 907-KokoEatingBananas/907-KokoEatingBananas.cpp
@@ -0,0 +1,30 @@
+// Last updated: 28/05/2026, 14:00:51
+class Solution {
+public:
+
+    bool canFinish(vector<int>& piles, int h, int speed) {
+
+        long long hours = 0;
+        for(int pile : piles) {
+            hours += (pile + speed - 1) / speed;
+        }
+        return hours <= h;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = *max_element(piles.begin(),piles.end());
+        int ans = high;
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+            if(canFinish(piles, h, mid)) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
