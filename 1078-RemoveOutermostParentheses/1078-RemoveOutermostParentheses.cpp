6e607ee604cfcdca32775837f26ae05fbaeb001d
Devx2107 <96907979+Devx2107@users.noreply.github.com>
2026-05-28 14:00:41 +0530

    Create 1078-RemoveOutermostParentheses

---
+++ 1078-RemoveOutermostParentheses/1078-RemoveOutermostParentheses.cpp
@@ -0,0 +1,22 @@
+// Last updated: 28/05/2026, 14:00:41
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+
+        string ans;
+        int depth = 0;
+        for(char c : s) {
+            if(c == '(') {
+                if(depth > 0)
+                    ans += c;
+                depth++;
+            }
+            else {
+                depth--;
+                if(depth > 0)
+                    ans += c;
+            }
+        }
+        return ans;
+    }
+};
